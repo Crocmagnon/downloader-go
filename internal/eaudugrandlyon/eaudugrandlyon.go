1f3722244da02c7bbdd36d79a93e2886819471ce
Gabriel Augendre <gabriel@augendre.info>
2025-02-01 00:01:27 +0100

    first draft of new eaudugrandlyon downloader

--- internal/eaudugrandlyon/eaudugrandlyon.go
+++ internal/eaudugrandlyon/eaudugrandlyon.go
@@ -1,11 +1,14 @@
 package eaudugrandlyon
 
 import (
+	"errors"
 	"fmt"
 	"github.com/Crocmagnon/downloader-go/internal/pw"
 	"github.com/playwright-community/playwright-go"
 	"io"
 )
+
+var errNotImplemented = errors.New("not implemented")
 
 func Run(stdout, stderr io.Writer, headless bool, username, password, dir string) error {
 	return pw.Run(stdout, stderr, headless, pw.BrowserFirefox, func(page playwright.Page) error {
@@ -26,17 +29,17 @@ func downloadFile(page playwright.Page, identifier, password, outputDir string) 
 }
 
 func login(page playwright.Page, identifier, password string) error {
-	_, err := page.Goto("https://www.eaudugrandlyon.com/default.aspx")
+	_, err := page.Goto("https://agence.eaudugrandlyon.com/#/login")
 	if err != nil {
 		return fmt.Errorf("going to: %w", err)
 	}
 
-	err = page.WaitForURL("https://www.eaudugrandlyon.com/default-connected.aspx", playwright.PageWaitForURLOptions{Timeout: playwright.Float(2000)})
+	err = page.WaitForURL("https://agence.eaudugrandlyon.com/#/tableau-de-bord", playwright.PageWaitForURLOptions{Timeout: playwright.Float(2000)})
 	if nil == err {
 		return nil // already logged in
 	}
 
-	if err := page.Locator("#login").Fill(identifier); err != nil {
+	if err := page.Locator("input[type=email]").Fill(identifier); err != nil {
 		return fmt.Errorf("typing identifier: %w", err)
 	}
 
@@ -44,7 +47,7 @@ func login(page playwright.Page, identifier, password string) error {
 		return fmt.Errorf("typing password: %w", err)
 	}
 
-	if err := page.Locator("input[type=submit][name=connect]").Click(); err != nil {
+	if err := page.Locator("button[type=submit]").Click(); err != nil {
 		return fmt.Errorf("clicking login button: %w", err)
 	}
 
@@ -52,11 +55,13 @@ func login(page playwright.Page, identifier, password string) error {
 }
 
 func downloadAndSave(page playwright.Page, outputDir string) error {
-	if _, err := page.Goto("https://www.eaudugrandlyon.com/mon-espace-compte-consulter-facture.aspx"); err != nil {
+	if _, err := page.Goto("https://agence.eaudugrandlyon.com/#/factures"); err != nil {
 		return fmt.Errorf("going to: %w", err)
 	}
 
-	return pw.Download(page, outputDir, func() error {
-		return page.Locator(".facture-access").First().Click()
-	})
+	return fmt.Errorf("%w: no invoice available when developing", errNotImplemented)
+
+	//return pw.Download(page, outputDir, func() error {
+	//	return page.Locator(".facture-access").First().Click()
+	//})
 }
